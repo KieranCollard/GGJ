a1e77f4fe7e0a1a2322da7eedcb00bd68fb47b0a
Kieran Collard <kierancollard@outlook.com>
2020-02-01 17:35:18 +1100

    Assets and base scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    Rigidbody body = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        Debug.Assert(body != null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        float x = horizontal * movementSpeed * Time.deltaTime;
+        float z = vertical * movementSpeed * Time.deltaTime;
+        Vector3 newPos = new Vector3(body.position.x + x, body.position.y, body.position.z + z);
+        body.MovePosition(newPos);
+    }
+}
