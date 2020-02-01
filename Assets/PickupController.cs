7775f9f6f31aab6f1156310fdd625ba1b233458b
Travis <travgw@hotmail.com>
2020-02-01 15:49:48 +1100

    Initial character controller with pickup and throw

---
+++ Assets/PickupController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    private List<GameObject> pickupsInRange;
+
+    [SerializeField]
+    private float chargeTime = 1.0f;
+
+    [SerializeField]
+    private float maxThrowPower = 250.0f;
+
+    [SerializeField]
+    private Transform pickupPoint;
+    GameObject pickedUpObject;
+
+    bool buildCharge = false;
+    float throwPower = 0;
+    float minThrowPower = 0.5f;
+    bool hasPickup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickupsInRange = new List<GameObject>();
+        hasPickup = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pickup"))
+        {
+            PickupObject();
+        }
+        else if (Input.GetButtonDown("Throw"))
+        {
+            buildCharge = true;
+        }
+        else if (Input.GetButtonUp("Throw"))
+        {
+            buildCharge = false;
+            ThrowObject();
+        }
+
+        if (buildCharge)
+        {
+            BuildCharge();
+        }
+
+        
+    }
+
+    private void BuildCharge()
+    {
+        float dt = Time.deltaTime;
+        float chargeRate = 1 / chargeTime;
+        float chargeAmount = dt * chargeRate;
+
+        throwPower += chargeAmount;
+        throwPower = Mathf.Min(throwPower, 1);
+    }
+
+    void PickupObject()
+    {
+        if (hasPickup || pickupsInRange.Count == 0)
+        {
+            return;
+        }
+
+        int rand = UnityEngine.Random.Range(0, pickupsInRange.Count);
+        pickedUpObject = pickupsInRange[rand];
+        pickupsInRange.Remove(pickedUpObject);
+
+        PickupObject pickup = pickedUpObject.GetComponentInParent<PickupObject>();
+
+        hasPickup = true;
+        pickup.Pickup(pickupPoint);
+    }
+
+    void ThrowObject()
+    {
+        if (!hasPickup)
+        {
+            return;
+        }
+
+        float actualThrowPower = Mathf.Max(minThrowPower, throwPower);
+        PickupObject pickup = pickedUpObject.GetComponentInParent<PickupObject>();
+        pickup.Throw(actualThrowPower, maxThrowPower);
+        throwPower = 0;
+        pickedUpObject = null;
+        hasPickup = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            pickupsInRange.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            GameObject collidingObject = other.gameObject;
+            for (int i = 0; i < pickupsInRange.Count; i++)
+            {
+                GameObject go = pickupsInRange[i];
+                if (go == collidingObject)
+                {
+                    pickupsInRange.Remove(go);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/PickupObject.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    bool inRange;
+    Rigidbody rb;
+    Transform parent;
+    float upModifier = 2;
+    float forwardModifier = 1;
+    float playerVelocityModifier = 2;
+    float objectMass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inRange = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pickup(Transform parent)
+    {
+        transform.parent = parent;
+        transform.localPosition = Vector3.zero;
+
+        rb.freezeRotation = true;
+        rb.detectCollisions = false;
+        rb.isKinematic = true;
+    }
+
+    // We want to pass the force here if we want to have any sort of throw power.
+    public void Throw(float throwPower, float maxThrowCharge)
+    {
+        Vector3 forceDirection = transform.parent.up + transform.parent.forward;
+        Rigidbody playerRb = transform.parent.GetComponentInParent<Rigidbody>();
+
+        Vector3 upForce = transform.parent.up * upModifier * maxThrowCharge * throwPower;
+        Vector3 forwardForce = transform.parent.forward * forwardModifier * maxThrowCharge * throwPower;
+        Vector3 impartedForce = playerRb.velocity * playerVelocityModifier;
+        Vector3 force = upForce + forwardForce + impartedForce;
+
+        transform.parent = null;
+        rb.isKinematic = false;
+        rb.freezeRotation = false;
+        rb.detectCollisions = true;
+        rb.AddForce(force);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = false;
+        }
+    }
+}
