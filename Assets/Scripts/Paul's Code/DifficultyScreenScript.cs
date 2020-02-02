fc2613b42a51cde2264178d81b220c8c6e96960b
Pnourish <pnourish@hotmail.com>
2020-02-02 12:29:13 +1100

    Paul commit 3, added read me screen, increased text size, and changed cube prefabs

---
+++ Assets/Scripts/Paul's Code/DifficultyScreenScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DifficultyScreenScript : MonoBehaviour
+{
+    private float changeScenePauseTime = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDifficulty1Selected()
+    {
+        SetDifficulty(1);
+        StartCoroutine(LoadGameScene("ReadMeScene"));
+    }
+    public void OnDifficulty2Selected()
+    {
+        SetDifficulty(2);
+        StartCoroutine(LoadGameScene("ReadMeScene"));
+    }
+    public void OnDifficulty3Selected()
+    {
+        SetDifficulty(3);
+        StartCoroutine(LoadGameScene("ReadMeScene"));
+    }
+
+
+    void SetDifficulty(int difficultyLevel)
+    {
+        PlayerPrefs.SetInt("difficulty", difficultyLevel);
+        PlayerPrefs.Save();
+    }
+
+    IEnumerator LoadGameScene(string sceneToLoad)
+    {
+        Debug.Log("Currently loading level: " + sceneToLoad);
+        yield return new WaitForSeconds(changeScenePauseTime);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
---
+++ Assets/Scripts/Paul's Code/ReadMeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReadMeScript : MonoBehaviour
+{
+    private float changeScenePauseTime = 1.0f;
+
+    public void OnContinueSelected()
+    {
+        StartCoroutine(LoadGameScene("Level01"));
+    }
+
+    IEnumerator LoadGameScene(string sceneToLoad)
+    {
+        Debug.Log("Currently loading level: " + sceneToLoad);
+        yield return new WaitForSeconds(changeScenePauseTime);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
